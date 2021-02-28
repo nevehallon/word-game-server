ff874afc80e66250c9bf3b89cddf78c4c98b2f70
Neveh Allon <34620850+nevehallon@users.noreply.github.com>
2021-02-28 12:34:08 +0200

    change cors settings

--- js/server.js
+++ js/server.js
@@ -24,8 +24,7 @@ corsOptions = {
   origin: process.env.BASE_URL || "https://nevehallon.github.io",
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
-// app.use(cors(corsOptions));
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.get("/wordFinder", (req, res, next) => {
   res.send({
