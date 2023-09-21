51855e733074f8ef976e1d8c8cc3988bc5aec656
nevehallon <nevehallon@gmail.com>
2023-09-22 00:13:11 +0300

    refactor and cleanup

--- server/index.ts
+++ server/index.ts
@@ -6,41 +6,50 @@ import { useMiddleWares } from './middlewares';
 
 const app = useMiddleWares(express());
 
+app.get(
+	'/',
+	async (_, res) => {
+		res.send('Hello World!');
+	}
+);
 
+app.get(
+	'/wordFinder',
+	(req, res,) => {
+		res.send({
+			wordsFound: wordFinder(
+				(req?.query?.letters as string) || '',
+				Number(req.query.numBlanks)
+			),
+		});
+	}
+);
 
-app.get('/wordFinder', (
-	req,
-	res,
-) => {
-	res.send({
-		wordsFound: wordFinder(
-			(req?.query?.letters as string) || '',
-			Number(req.query.numBlanks)
-		),
-	});
-});
+app.get(
+	'/wordTrieStr',
+	(_, res) => {
+		res.send({
+			wordTrieStr,
+			reverseWordTrieStr,
+		});
+	}
+);
 
-app.get('/wordTrieStr', (req, res, next) => {
-	res.send({
-		wordTrieStr,
-		reverseWordTrieStr,
-	});
-});
+app.get(
+	'/defineWords',
+	async (req, res) => {
+		let words = (req.query.words as string).replace(/ */g, '').split(',');
+		res.send({
+			words: await defineWordArr(words),
+		});
+	}
+);
 
-app.get('/defineWords', async (req, res) => {
-	let words = (req.query.words as string).replace(/ */g, '').split(',');
-	res.send({
-		words: await defineWordArr(words),
-	});
-});
-
-app.get('/', async (req, res) => {
-	res.send('Hello World!');
-});
-
-app.all('*', (req, res) => {
-	res.send('404: page not found');
-});
+app.all('*',
+	(_, res) => {
+		res.send('404: page not found');
+	}
+);
 
 const PORT = process.env.PORT || 80
 app.listen(
